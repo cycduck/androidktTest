81098a0bf8f0cccb42566256e0ead4641de673b8
Kelly K H <kh.waterloo@gmail.com>
2020-05-03 16:54:44 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React, { Component } from 'react'
-import './App.scss'
+import React, { Component } from "react";
+import "./App.scss";
+import linkedinIcon from "./asset/icon/linkedin.svg";
+import gitIcon from "./asset/icon/github-black.svg";
+import twitterIcon from "./asset/icon/twitter.svg";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
-        <div className="app__container">
-          <div>
-            <h1>
-              Kelly Ho
-            </h1>
-            <h2>
-              Hi, I'm Kelly
-            </h2>
+      <section className="landing">
+        <div className="landing__hero">
+          <div className="landing__title-side">
             <div>
-              <a href="https://www.linkedin.com/in/kellykho/">LinkedIn</a>
-              <a href="https://github.com/cycduck">git</a>
+              <h1 className="landing__title">Kelly K Ho</h1>
+              <p className="landing__tagline">a cyclist developer</p>
+              <div className="landing__link">
+                <a className="landing__a" href="https://twitter.com/KawaHaraPhotos">
+                  <img className="landing__icon" src={twitterIcon} alt="Twitter" />
+                </a>
+                <a className="landing__a" href="https://www.linkedin.com/in/kellykho/">
+                  <img className="landing__icon" src={linkedinIcon} alt="Linkedin" />
+                </a>
+                <a className="landing__a" href="https://github.com/cycduck">
+                  <img className="landing__icon" src={gitIcon} alt="Github" />
+                </a>
+              </div>
+            </div>
+  
+            <div className="landing__divider" />
+  
+            <div className="landing__project-side">
+              <h2 className="landing__section">projects</h2>
+              <ul className="landing__ul">
+                <li className="landing__li">
+                  <a className="landing__a" href="https://github.com/cycduck/ttc">
+                    Real time transit map
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
-
-          <div className="app__divider" />
-
-          <div>
-            <h2>projects</h2>
-            <ul>
-              <li><a href="https://github.com/cycduck/ttc">ttc</a></li>
-            </ul>
-          </div>
         </div>
-      </div>
-    )
+      </section>
+    );
   }
 }
