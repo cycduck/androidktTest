c7dd50422acb60c2f565c7ab915a3fa33d56507f
jason tlh <jasonjdh94@gmail.com>
2020-05-03 14:35:38 -0400

    advanced setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import './App.scss'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        hi
+      <div className="app">
+        <div className="app__container">
+          <div>
+            <h1>
+              Kelly Ho
+            </h1>
+            <h2>
+              Hi, I'm Kelly
+            </h2>
+            <div>
+              <a href="https://www.linkedin.com/in/kellykho/">LinkedIn</a>
+              <a href="https://github.com/cycduck">git</a>
+            </div>
+          </div>
+
+          <div className="app__divider" />
+
+          <div>
+            <h2>projects</h2>
+            <ul>
+              <li><a href="https://github.com/cycduck/ttc">ttc</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
